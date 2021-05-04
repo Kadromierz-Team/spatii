cdcbbc8d431ada6dc644f92be88390480a948a9b
Jaco <jszczerbinski@pm.me>
2021-05-04 16:47:37 +0200

    modal try

--- src/views/ResourcesView/ResourcesView.jsx
+++ src/views/ResourcesView/ResourcesView.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { PageHeader, Checkbox, Tag } from 'antd';
+import { PageHeader, Checkbox, Tag, Modal } from 'antd';
 import { Filters } from '../../components/organisms';
 import { Table, Button } from '../../components/molecules';
 import { getColumns } from './utils';
@@ -12,18 +12,21 @@ const data = [
     isSelected: false,
     imageTag: '00000000',
     status: 'OK',
+    options: 'POD1',
   },
   {
     key: '2',
     name: 'POD2',
     isSelected: true,
     status: 'NOT OK',
+    options: 'POD2',
   },
   {
     key: '2',
     name: 'POD2',
     isSelected: true,
     status: 'NO INFO',
+    options: 'POD3',
   },
 ];
 
@@ -34,6 +37,22 @@ const ResourcesView = ({
   changeNamespaces,
   changeResourceTypes,
 }) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [resourceName, setResourceName] = useState(null);
+  const showModal = (value) => {
+    setIsModalVisible(true);
+    setResourceName(value);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+    setResourceName(null);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+    setResourceName(null);
+  };
   console.log(filters);
   return (
     <div className="resource-view-wrapper">
@@ -44,7 +63,17 @@ const ResourcesView = ({
         changeNamespaces={changeNamespaces}
         changeResourceTypes={changeResourceTypes}
       />
-      <Table columns={getColumns('pod')} data={data} />
+      <Table columns={getColumns('pod', showModal)} data={data} />
+      <Modal
+        title="Basic Modal"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>{resourceName}</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
       <div></div>
     </div>
   );
