230d02500146f3bad88d7aa8db54b5111ea03468
AdamOleszko <adamoleszko44@gmail.com>
2021-05-04 13:15:37 +0200

    merge issues

--- src/components/ResourcesView/ResourcesView.jsx
+++ src/components/ResourcesView/ResourcesView.jsx
@@ -1,25 +1,15 @@
 import React from 'react';
 import { PageHeader, Select } from 'antd';
 import './ResourceView.scss';
+import { Filters } from '../molecules';
 
 const { Option } = Select;
 
 const ResourcesView = () => {
   return (
     <div>
-      <PageHeader
-        title="Spatii"
-        extra={[
-          <Select key="namespaceFilter" value="frontend" style={{ width: 124 }}>
-            <Option value="production" key="production">
-              production
-            </Option>
-            <Option value="frontend" key="frontend">
-              frontend
-            </Option>
-          </Select>,
-        ]}
-      />
+      <PageHeader title="Spatii" />
+      <Filters />
     </div>
   );
 };
