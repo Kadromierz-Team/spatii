b928dfe768b9de2cdc5bba09cfa35aa7cbd200c2
Jaco <jszczerbinski@pm.me>
2021-05-04 15:09:57 +0200

    new methods to kubeclt service

--- src/services/KubectlService.js
+++ src/services/KubectlService.js
@@ -3,23 +3,51 @@ import execa from 'execa';
 class KubectlService {
   async getContexts() {
     const args = ['config', 'get-contexts', '--no-headers=true', '-o=name'];
-    const results = await this.execute(args);
+    const results = await this._execute(args);
     return results.split('\n');
   }
 
   async changeContext(context) {
     const args = ['config', 'use-context', context];
-    await this.execute(args);
+    await this._execute(args);
+  }
+
+  async getCurrentContext() {
+    const args = ['config', 'current-context'];
+    return this._execute(args);
+  }
+
+  async getNamespaceResources(namespace, resource) {
+    const args = ['get', resource, `-n=${namespace}`, '-o=json'];
+    const results = await this._execute(args);
+    const parsedResources = JSON.parse(results);
+    return parsedResources.items;
+  }
+
+  async getContextResources() {
+    const args = [
+      'api-resources',
+      '--namespaced=true',
+      '--no-headers=true',
+      '-o=name',
+    ];
+    const results = await this._execute(args);
+    const resourcesArray = results.split('\n');
+    return this._getUsableResourceName(resourcesArray);
   }
 
   async getNamespaces() {
     const args = ['get', 'namespaces', '-o=json'];
-    const results = await this.execute(args);
+    const results = await this._execute(args);
     const parsedResults = JSON.parse(results);
     return parsedResults.items.map((item) => item.metadata.name);
   }
 
-  async execute(args) {
+  _getUsableResourceName(resources) {
+    return resources.map((resource) => resource.split('.')[0]);
+  }
+
+  async _execute(args) {
     try {
       const { stdout } = execa('kubectl', args);
       return stdout;
